ca2a9aec5013a4564a8778ef9f804561cdf84617
Airdrift <130802265+AirDriftOFF@users.noreply.github.com>
2023-04-15 00:50:41 +0200

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,9 @@
+__CONFIG__ = {
+    'webhook' : True,
+    'antidebug' : True,
+    'browsers' : True,
+    'discordtoken' : True,
+    'injection' : True,
+    'startup' : True,
+    'systeminfo' : True,
+}
